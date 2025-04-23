6ce6d6e4a36d181145a082e7963f42671628fe4a
Davit Mas <71844784+Davirre2@users.noreply.github.com>
2025-04-23 16:59:33 +0200

    Javascript Learning

---
+++ home.js
@@ -0,0 +1,22 @@
+showMessage('BON SANT JORDIIII');
+console.log("Tinc son omg");
+
+const noCanvia = 30;
+
+let total = 149.99,
+ product = 'Hiking Boots',
+ discounted = true;
+//després d'un primer let, fiques comes i no més let
+
+console.log(product);
+console.log(typeof product);
+
+total = total + 0.01;
+let remainder = total % 4,
+    difference = total - 149.99;
+showMessage(difference);
+//on a un commentaire ici, c'est petit
+
+/*
+omg multiline comments!!!
+*/
